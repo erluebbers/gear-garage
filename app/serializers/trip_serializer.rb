c8d46bdffe6dfd44a8359827ebf9228f382ebf51
Erik Luebbers <erluebbers@gmail.com>
2022-07-20 13:41:52 -0700

    add items to trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,5 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :trip_description
+  attributes :id, :title, :year, :trip_description, :items
   belongs_to :user
   has_many :packlists
   has_many :items, through: :packlists
