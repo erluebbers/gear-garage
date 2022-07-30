f12b1b736a5fc5760d9e930cf17770759b6288b1
Erik Luebbers <erluebbers@gmail.com>
2022-07-29 20:22:11 -0700

    add delete function and state updating

--- client/src/features/Trips/TripCard.js
+++ client/src/features/Trips/TripCard.js
@@ -4,11 +4,28 @@ import React from "react";
 
 
 function TripCard( {trip} ) {
-  const {title, trip_description, year, items} = trip
+  const {title, trip_description, year, items, id} = trip
 
   const associatedItems = items.map(item => {
     return <li key={item.id}>{item.name}</li>
   })
+
+    //Delete a trip
+    const handleDeleteTrip = (id) => {
+      fetch(`/users/${user.id}/trips/${id}`,{
+        method: 'DELETE'
+      })
+      .then(() => onDeleteTrip(id))
+    }
+  
+    //update Gear List after deleting item
+    const onDeleteTrip = (deletedId) => {
+      const updatedItems = gear.filter((item) => deletedId !== item.id)
+      setGear(updatedItems)
+    }
+
+
+
 
   return (
     <div className="trip-card">
@@ -20,7 +37,7 @@ function TripCard( {trip} ) {
         <li>Items assigned to this Trip:</li>
         {items.length === 0 ? <li>No items yet!</li> : associatedItems}
       </ul>
-      <button>Delete Trip</button>
+      <button onClick={() => handleDeleteTrip(id)}>Delete Trip</button>
     </div>
   );
 }
