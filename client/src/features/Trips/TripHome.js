9c5a3618aa758cde5848d130c3c06a950dd6abc0
Erik Luebbers <erluebbers@gmail.com>
2022-07-31 21:12:15 -0700

    edit trip home formatting

--- client/src/features/Trips/TripHome.js
+++ client/src/features/Trips/TripHome.js
@@ -6,19 +6,19 @@ import TripList from "./TripList"
 
 function TripHome( {tripList, setTripList, user} ) {
 
-     //Delete a trip
-     const handleDeleteTrip = (id) => {
-      fetch(`/users/${user.id}/trips/${id}`,{
-        method: 'DELETE'
-      })
-      .then(() => onDeleteTrip(id))
-    }
-  
-    //update Trip List after deleting trip
-    const onDeleteTrip = (id) => {
-      const updatedTrips = tripList.filter((trip) => id !== trip.id)
-      setGear(updatedItems)
-    }
+    //Delete a trip
+    const handleDeleteTrip = (id) => {
+    fetch(`/users/${user.id}/trips/${id}`,{
+      method: 'DELETE'
+    })
+    .then(() => onDeleteTrip(id))
+  }
+
+  //update Trip List after deleting trip
+  const onDeleteTrip = (id) => {
+    const updatedTrips = tripList.filter((trip) => id !== trip.id)
+    setGear(updatedItems)
+  }
 
   return (
     <div>
